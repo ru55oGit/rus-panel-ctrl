ad493a7fda2359d212c82cdc0fa271318e4444e9
Patricio Toledo <patricio.toledo@MacBook-Air-de-Patricio.local>
2023-03-31 13:27:54 -0300

    Boilerplate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles'
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom'
+import IntroPage from '../src/pages/introPage'
+import theme from './theme'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Routes>
+            <Route element={<IntroPage />} path="/" />
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </StyledEngineProvider>
+  )
 }
 
-export default App;
+export default App
